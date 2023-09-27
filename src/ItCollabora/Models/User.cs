4d81bf018de4a26f593f1a5872539a700ee6048d
Maria Almeida <maria.almeida@atomoshyla.com>
2023-09-27 00:57:16 -0300

    Model | user

---
+++ src/ItCollabora/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+using System.Diagnostics;
+using System.Net;
+using System.Numerics;
+using System.Text.Json.Serialization;
+
+namespace ItCollabora.Models
+{
+    public class User
+    {
+            [Key]
+            public Guid UserId { get; set; } = new Guid();
+            [Required, MaxLength(50)]
+            public string Name { get; set; }
+            [Required, MaxLength(50)]
+            public string Email { get; set; }
+            [Required, MaxLength(50), JsonIgnore]
+            public string EncryptedPassword { get; set; } = Guid.Empty.ToString();
+}
+}
