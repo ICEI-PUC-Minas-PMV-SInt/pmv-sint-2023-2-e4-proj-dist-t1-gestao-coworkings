d0504151dae7b1fd4f706ddaa98c651a6c4139d1
Cristhyam-Augusto <cristhyamparaiso@gmail.com>
2023-10-02 19:28:18 -0300

    feat: add Rent and Room models

---
+++ src/ItCollabora/Models/Room.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ItCollabora.Models
+{
+    
+    public class Room
+    {
+        [Key]
+        public Guid RoomId { get; set; } = Guid.NewGuid();
+
+        [Required, MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Description { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [Required]
+        public int TotalCapacity { get; set; }
+
+        [ForeignKey("OwnerUserId")]
+        public Guid OwnerUserId { get; set; }
+
+        public User OwnerUser { get; set; }
+
+        public ICollection<User> ReservedByUsers { get; set; } = new List<User>();
+    }
+}
--- src/ItCollabora/Models/User.cs
+++ src/ItCollabora/Models/User.cs
@@ -4,18 +4,27 @@ using System.Diagnostics;
 using System.Net;
 using System.Numerics;
 using System.Text.Json.Serialization;
+using ItCollabora.Models;
 
 namespace ItCollabora.Models
 {
     public class User
     {
-            [Key]
-            public Guid UserId { get; set; } = new Guid();
-            [Required, MaxLength(50)]
-            public string Name { get; set; }
-            [Required, MaxLength(50)]
-            public string Email { get; set; }
-            [Required, MaxLength(50), JsonIgnore]
-            public string EncryptedPassword { get; set; } = Guid.Empty.ToString();
-}
+        [Key]
+        public Guid UserId { get; set; } = Guid.NewGuid();
+
+        [Required, MaxLength(50)]
+        public string Name { get; set; }
+
+        [Required, MaxLength(50)]
+        public string Email { get; set; }
+
+        [Required, MaxLength(50), JsonIgnore]
+        public string EncryptedPassword { get; set; } = Guid.Empty.ToString();
+
+        public ICollection<Room> OwnRooms { get; set; } = new List<Room>();
+
+        public ICollection<Room> RoomBookings { get; set; } = new List<Room>();
+    }
+
 }
