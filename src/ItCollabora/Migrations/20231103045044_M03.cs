5c1d7f06b48744ce7eedee3cc8f6e351ab8e29ed
Leticia.Lopes <l.lopes@accenture.com>
2023-11-03 03:05:33 -0300

    Add Migrations

---
+++ src/ItCollabora/Migrations/20231103045044_M03.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ItCollabora.Migrations
+{
+    /// <inheritdoc />
+    public partial class M03 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Rents",
+                columns: table => new
+                {
+                    IdRent = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    IdUser = table.Column<int>(type: "int", nullable: false),
+                    IdRoom = table.Column<int>(type: "int", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rents", x => x.IdRent);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rents");
+        }
+    }
+}
