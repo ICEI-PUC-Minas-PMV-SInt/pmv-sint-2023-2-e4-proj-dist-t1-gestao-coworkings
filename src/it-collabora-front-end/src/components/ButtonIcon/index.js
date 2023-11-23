60033baf15c0a1d0329f5a60ce075731619c04ce
leticia.lopes <l.lopes@accenture.com>
2023-11-22 23:20:41 -0300

    Create Component Navbar

---
+++ src/it-collabora-front-end/src/components/ButtonIcon/index.js
@@ -0,0 +1,19 @@
+import './styles.css';
+
+//import { ReactComponent as ArrowIcon } from 'assets/images/arrow.svg';
+
+
+
+const ButtonIcon = () => {
+  return (
+    <div className="btn-container">
+      <button className="btn btn-primary">
+        <h6>teste</h6>
+      </button>
+      <div className="btn-icon-container">
+      </div>
+    </div>
+  );
+};
+
+export default ButtonIcon;
---
+++ src/it-collabora-front-end/src/pages/Home/index.js
@@ -0,0 +1,30 @@
+import { ReactComponent as MainImage } from 'C:/Users/l.lopes/Downloads/PUC2/pmv-sint-2023-2-e4-proj-dist-t1-gestao-coworkings/src/it-collabora-front-end/src/assets/images/home-image.svg';
+import ButtonIcon from '../../components/ButtonIcon';
+// import Navbar from '../../components/Navbar';
+
+import './styles.css';
+
+const Home = () => {
+  return (
+    <>
+     {/* <Navbar /> */}
+      <div className="home-container">
+        <div className="base-card home-card">
+          <div className="home-content-container">
+            <div>
+              <h1>Bem-vindo à Nova Era da Produtividade</h1>
+              <p>Explore nosso catálogo de espaços que transformarão sua experiência de trabalho.</p>
+              <ButtonIcon />
+            </div>
+          </div>
+          <div className="home-image-container">
+            <MainImage />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+
+export default Home;
