200f22d015001da2fd2fd8ef0d5e1d9b1a685a7e
leticia.lopes <l.lopes@accenture.com>
2023-11-24 01:35:13 -0300

    Add routes navbar

--- src/it-collabora-front-end/src/Routes.js
+++ src/it-collabora-front-end/src/Routes.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import Home from './pages/Home';
 import Catalog from './pages/Catalog';
+import Login from './pages/Login';
 import Navbar from './components/Navbar';
 
 const MainRoutes = () => (
@@ -10,6 +11,7 @@ const MainRoutes = () => (
     <Routes>
       <Route path="/" element={<Home />} /> 
       <Route path="/catalog" element={<Catalog />} /> 
+      <Route path="/login" element={<Login />} /> 
     </Routes>
   </BrowserRouter>
 );
--- src/it-collabora-front-end/src/components/Navbar/index.js
+++ src/it-collabora-front-end/src/components/Navbar/index.js
@@ -1,14 +1,15 @@
 import './styles.css';
-//import '@popperjs/core';
 import 'bootstrap/js/src/collapse';
+
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
       <div className="container-fluid">
-        <a href="link" className="nav-logo-text">
+        <Link to="/" className="nav-logo-text">
           <h4>It Collabora</h4>
-        </a>
+        </Link>
 
         <button
           className="navbar-toggler"
@@ -25,15 +26,13 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="dscatalog-navbar">
           <ul className="navbar-nav offset-md-2 main-menu">
             <li>
-              <a href="link" className="active">
-                HOME
-              </a>
+              <NavLink to="/" activeClassName="active" exact>HOME</NavLink>
             </li>
             <li>
-              <a href="link">CATÁLOGO</a>
+              <NavLink to="/catalog" activeClassName="active">CATÁLOGO</NavLink>
             </li>
             <li>
-              <a href="link">ADMIN</a>
+              <NavLink to="/login" activeClassName="active">LOGIN</NavLink>
             </li>
           </ul>
         </div>
